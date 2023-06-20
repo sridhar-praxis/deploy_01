e8e4225cdf157973dce3d283557723818551d330
Vol Ume <vol3ume4@gmail.com>
2023-06-20 21:42:00 +0530

    Update app.py
    small tweaks

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ batch_names = ["Jan 2022", "July 2022", "Jan 2023", "July 2023"]
 
 selected_batch = st.sidebar.selectbox("Select Batch", batch_names)
 
-st.title(f"Hello, {selected_batch} batch!")
+st.title(f"Hello!!, {selected_batch} batch!")
 st.write("This is a Streamlit web app.")
 
 var = st.sidebar.slider("How is the josh?", 10, 50, 20, 1)
